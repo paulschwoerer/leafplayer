0d388d6ea2c888f701a9b96679933da339395f22
PAUL.SCHWORER <paul.schworer@mediamonks.com>
2017-12-12 15:25:00 +0100

    Move subdirectory configuration to config/index.js

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -1,13 +1,15 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path')
 
+var argv = require('minimist')(process.argv.slice(2));
+
 module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: typeof argv.base === 'string' ? argv.base : '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
