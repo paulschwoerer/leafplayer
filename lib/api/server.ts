3f15784ee876d8826fa7aa0d4ab1fd854a1fee65
Paul Schwörer <hello@paulschwoerer.de>
2022-02-27 23:23:28 +0100

    fix: correct path to static frontend files

--- lib/api/server.ts
+++ lib/api/server.ts
@@ -12,7 +12,7 @@ export async function initServer(
   const server = fastify({ logger: false });
 
   await server.register(fastifyStatic, {
-    root: path.join(__dirname, '../public'),
+    root: path.join(__dirname, '../../public'),
   });
 
   server.setNotFoundHandler(async (_, reply) => {
