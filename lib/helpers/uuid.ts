2d6c7aa0b0137af438fff7b005db568cbed3e5ed
Paul Schwörer <hello@paulschwoerer.de>
2026-06-08 20:52:29 +0200

    refactor: use node crypto uuids

--- lib/helpers/uuid.ts
+++ lib/helpers/uuid.ts
@@ -1,10 +1,10 @@
-import { v4 as uuidv4 } from 'uuid';
+import { randomUUID } from 'crypto';
 
 const UUID_V4_REGEX =
   /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
 
 export function generateUuid(): string {
-  return uuidv4();
+  return randomUUID();
 }
 
 export function isUuidV4(maybeUuidV4: string): boolean {
